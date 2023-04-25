779bfd4d7adde3e6779c44108ef74bcd3b381912
MaiJonathan <jonathanbmai@gmail.com>
2023-04-25 09:26:06 -0700

    Added Home Fragment for settings, notifications, and home

--- app/src/main/java/com/example/fakecall/MainActivity.kt
+++ app/src/main/java/com/example/fakecall/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.fakecall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fakecall.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.notification -> replaceFragment(Profile())
+                R.id.settings -> replaceFragment(Settings())
+                else -> super.onOptionsItemSelected(it)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
