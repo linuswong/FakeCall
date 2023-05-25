c241334478d46234b98e108510b3f1fe7a8c03fb
per2 <linuswong9@gmail.com>
2023-05-25 09:10:30 -0700

    mute button change color

--- app/src/main/java/com/example/fakecall/CallScreenActivity.kt
+++ app/src/main/java/com/example/fakecall/CallScreenActivity.kt
@@ -14,5 +14,9 @@ class CallScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
+
+
+
     }
 }
--- app/src/main/java/com/example/fakecall/FakeCallScreen.kt
+++ app/src/main/java/com/example/fakecall/FakeCallScreen.kt
@@ -1,13 +1,18 @@
 package com.example.fakecall
+
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.fakecall.databinding.FakeCallScreenBinding
-import kotlinx.coroutines.Dispatchers.Main
+
 
 class FakeCallScreen:AppCompatActivity() {
 
     private lateinit var binding: FakeCallScreenBinding
+    private var thingy: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,19 +20,24 @@ class FakeCallScreen:AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.floatingActionButtonMuteFakeCallScreen.setOnClickListener {
+            var mute = binding.floatingActionButtonMuteFakeCallScreen
+            if (!thingy) {
+                mute.getBackground().mutate()
+                    .setTint(ContextCompat.getColor(it.context, R.color.white))
+                thingy = true
+            } else if (thingy) {
+                mute.getBackground().mutate()
+                    .setTint(ContextCompat.getColor(it.context, R.color.balls))
+                thingy = false
 
-        binding.floatingActionButtonMuteFakeCallScreen.setOnClickListener{
-            var mute=binding.floatingActionButtonMuteFakeCallScreen
-
-            mute.setBackgroundColor(0)
+            }
+            binding.floatingActionButtonHangUp.setOnClickListener {
+                val detailIntent = Intent(it.context, CallActivity::class.java)
+                startActivity(detailIntent)
+            }
         }
-        binding.floatingActionButtonHangUp.setOnClickListener{
-            val detailIntent= Intent(it.context,CallActivity::class.java)
-            startActivity(detailIntent)
 
 
-        }
     }
-
-
 }
